4c6b836a7ce12098557e4d887ab4f76cfbcd460d
Neal Hardesty <nealhardesty@yahoo.com>
2018-04-22 10:38:34 -0700

    Enable sudo for user

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -3,6 +3,9 @@ if [ ! $(id "$USERNAME" >> /dev/null 2>&1) ]; then
   echo Creating user $USERNAME
   adduser -D "$USERNAME"
   echo "$USERNAME:$PASSWORD" | chpasswd
+
+  echo Enabling sudo for $USERNAME
+  echo "$USERNAME ALL=(ALL:ALL) ALL" >> /etc/sudoers
 fi
 
 ssh-keygen -A 
